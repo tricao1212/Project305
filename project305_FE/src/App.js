f9d28c37c0b80137edbd8e454610d2227eb72719
phucdang2002 <131520774+phucdang2002@users.noreply.github.com>
2024-05-31 20:54:28 +0700

    create patient

--- project305_FE/src/App.js
+++ project305_FE/src/App.js
@@ -1,10 +1,11 @@
 import Admin from "./pages/Admin";
 import CreateAccount from "./pages/CreateAccount";
+import CreatePatient from "./pages/CreatePatient";
 import Login from "./pages/Login";
 
 function App() {
   return (
-    <CreateAccount/>
+    <CreatePatient/>
   );
 }
 
