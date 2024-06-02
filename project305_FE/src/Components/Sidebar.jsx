566318bf508c9c87a05aa76a3725d89da6e4f4b9
vituan161 <vituan161@gmail.com>
2024-06-03 06:27:20 +0700

    adding homepages,side bar
    adding patient hompages, additional sidebar component

---
+++ project305_FE/src/Components/Sidebar.jsx
@@ -0,0 +1,55 @@
+
+import { useContext, createContext, useState } from "react"
+
+const SidebarContext = createContext()
+
+export default function Sidebar({ children }) {
+  
+  return (
+    <aside className="h-screen">
+      <nav className="h-full flex flex-col bg-[#cae9ec] border-r shadow-md">
+        <div className="p-4 pb-2 flex justify-center items-center">
+          <img
+            src="https://seeklogo.com/images/H/hospital-clinic-plus-logo-7916383C7A-seeklogo.com.png"
+            className={`overflow-hidden transition-all w-32`}
+            alt=""
+          />
+        </div>
+
+        <SidebarContext.Provider>
+          <ul className="flex-1 px-3">{children}</ul>
+        </SidebarContext.Provider>
+
+        <div className="flex p-3">
+        <button className="flex-1 bg-red-500 text-white py-2 px-3 rounded-md hover:shadow-md">
+          Logout
+        </button>
+      </div>
+      </nav>
+    </aside>
+  )
+}
+
+export function SidebarItem({ icon, text, alert }) {
+  return (
+    <li
+      className={`
+        relative flex items-center py-2 px-3 my-3
+        font-medium rounded-md cursor-pointer
+        transition-colors group bg-white hover:shadow-md
+    `}
+    >
+      {icon}
+      <span
+        className={`overflow-hidden transition-all w-52 ml-3`}
+      >
+        {text}
+      </span>
+      {alert && (
+        <div
+          className={`absolute right-2 w-2 h-2 rounded bg-indigo-400`}
+        />
+      )}
+    </li>
+  )
+}
--- project305_FE/src/index.js
+++ project305_FE/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Login from './pages/Login';
+import { PatientHome,DoctorHome, AdminHome } from './pages/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+    <PatientHome />
   </React.StrictMode>
 );
 
