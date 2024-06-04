da4faa738359729984b288e8456270427e30a720
phucdang2002 <131520774+phucdang2002@users.noreply.github.com>
2024-06-04 19:03:10 +0700

    add protected

--- project305_FE/src/App.js
+++ project305_FE/src/App.js
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Admin from "./pages/admin/Admin";
 import Login from "./pages/Login";
 import Home from "./pages/patient/Home";
+import Protected from "./Components/Protected";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login/>}/>
-      <Route path="/admin/*" element={<Admin/>}/>
-      <Route path="/patient/*" element={<Home/>}/>
+      <Route path="/admin/*" element={<Protected><Admin/></Protected>}/>
+      <Route path="/patient/*" element={<Protected><Home/></Protected>}/>
     </Routes>
   );
 }
